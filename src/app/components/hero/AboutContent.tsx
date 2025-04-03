f73deace8ce69713738284e0155ca0b157f883c6
msrissaxox <marissa.bianca.lamothe@gmail.com>
2025-04-03 00:15:05 -0400

    edit about me

--- src/app/components/hero/AboutContent.tsx
+++ src/app/components/hero/AboutContent.tsx
@@ -2,22 +2,30 @@ import React from 'react';
 
 export default function AboutContent() {
   return (
-    <div>
-      <p>
-        Former educator turned self-taught web developer based in Orlando,
-        Florida. I thrive on building exciting projects that push me beyond my
-        comfort zone and fuel continuous growth. My journey from education to
-        development has given me a unique perspective on problem-solving and a
-        passion for creating intuitive, user-focused experiences. I specialize
-        in <b>JavaScript</b>, <b>React</b>, <b>Node.js</b>, <b>Next.js</b>, and <b>SQL</b>, while constantly
-        exploring new libraries and staying current with the latest web
-        development trends. I'm currently channeling my teaching background into
-        building a <b>JavaScript course</b> to help others discover the power and
-        possibilities of programming. If you'd like to learn more about this course, click here. 
-        This project combines my love for education with my technical expertise. 
-        When I'm not coding, you'll find me exploring Orlando's vibrant art scene, 
-        working out, or reading. Let's build something amazing <b>together</b>.
-      </p>
-    </div>
+<div>
+  <p>
+    Former educator turned self-taught web developer based in Orlando,
+    Florida. I thrive on building exciting projects that push me beyond my
+    comfort zone and fuel continuous growth. My journey from education to
+    development has given me a unique perspective on problem-solving and a
+    passion for creating intuitive, user-focused experiences. I specialize
+    in <b>JavaScript</b>, <b>React</b>, <b>Node.js</b>, <b>Next.js</b>, and <b>SQL</b>, while constantly
+    exploring new libraries and staying current with the latest web
+    development trends. 
+
+    I am also skilled in <b>API calls</b> and have recently delved into <b>AI</b>, exploring its 
+    potential in web development. One of my recent projects, the <b>LinkedIn Post Generator</b>, 
+    showcases how I leveraged <b>OpenAI</b> to automate content creation, demonstrating my ability to 
+    integrate AI-driven solutions into my work.
+
+    I'm currently channeling my teaching background into
+    building a <b>JavaScript course</b> to help others discover the power and
+    possibilities of programming. If you'd like to learn more about this course, click here. 
+    This project combines my love for education with my technical expertise. 
+
+    When I'm not coding, you'll find me exploring Orlando's vibrant art scene, 
+    working out, or reading. Let's build something amazing <b>together</b>.
+  </p>
+</div>
   );
 }
