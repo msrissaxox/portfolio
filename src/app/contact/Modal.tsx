bc9d99fe6192441b1a9110b15f26134606872c98
msrissaxox <marissa.bianca.lamothe@gmail.com>
2025-05-08 23:54:39 -0400

    modal responsiveness

--- src/app/contact/Modal.tsx
+++ src/app/contact/Modal.tsx
@@ -8,7 +8,8 @@ const style = {
   top: '50%',
   left: '50%',
   transform: 'translate(-50%, -50%)',
-  width: 400,
+  width: '90vw', //Responsive width
+  maxWidth: 400, // Max width for larger screens
   bgcolor: 'background.paper',
   border: '2px solid #000',
   boxShadow: 24,
