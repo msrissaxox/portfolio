200a5d0154ab34f107189906886e1d0f1148aaa3
msrissaxox <marissa.bianca.lamothe@gmail.com>
2025-06-03 22:48:32 -0400

    update card in works and about me

--- src/app/components/TechStackandActivities/Works.tsx
+++ src/app/components/TechStackandActivities/Works.tsx
@@ -6,29 +6,66 @@ export default function Works() {
   return (
     <div className="mt-10 py-4">
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-6">
-        <a href="https://github.com/msrissaxox/blog#readme" target="_blank">
-          <TiltedCard imageSrc="/blog.gif" overlayContent="BackBook Blogs" />
+        <a
+          href="https://github.com/msrissaxox/blog#readme"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <TiltedCard
+            imageSrc="/blog.gif"
+            overlayContent="BackBook Blogs"
+            altText="BackBook Blogs gif"
+          />
         </a>
-        <a href="https://github.com/msrissaxox/starbrew/#readme" target="_blank">
+        <a
+          href="https://github.com/msrissaxox/starbrew/#readme"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           <TiltedCard
             imageSrc="/starbrew.gif"
             overlayContent="StarBrew Coffee"
+            altText="StarBrew Coffee gif"
           />
         </a>
-        <a href="https://github.com/msrissaxox/linkedinpostgen/tree/main/#readme" target="_blank">
+        <a
+          href="https://github.com/msrissaxox/linkedinpostgen/tree/main/#readme"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           <TiltedCard
             imageSrc="/linkedin.gif"
             overlayContent="LinkedIn AI Post Generator"
+            altText="LinkedIn AI Post Generator gif"
           />
         </a>
-        <a href="https://github.com/msrissaxox/fruits-frontend/tree/main/#readme" target="_blank">
+        <a
+          href="https://github.com/msrissaxox/fruits-frontend/tree/main/#readme"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           <TiltedCard
             imageSrc="/fruitfinder.gif"
             overlayContent="Fruit Finder API"
-          />{' '}
+            altText="Fruit Finder API gif"
+          />
         </a>
-        <TiltedCard imageSrc="/comingsoon.png" overlayContent="Coming Soon" />
-        <TiltedCard imageSrc="/comingsoon.png" overlayContent="Coming Soon" />
+        <a
+          href="https://www.goldenlightdesigns.co"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <TiltedCard
+            imageSrc="/gld.gif"
+            overlayContent="Golden Light Designs"
+            altText="Golden Light Designs gif"
+          />
+        </a>
+        <TiltedCard
+          imageSrc="/comingsoon.png"
+          overlayContent="Coming Soon"
+          altText="Coming Soon image"
+        />
       </div>
     </div>
   );
--- src/app/components/hero/AboutContent.tsx
+++ src/app/components/hero/AboutContent.tsx
@@ -1,28 +1,34 @@
 import React from 'react';
+import type { SpringOptions } from 'framer-motion';
 
 export default function AboutContent() {
   return (
     <div>
       <p>
-        Former educator turned self-taught Software Engineer based in Orlando,
-        Florida. I thrive on building exciting projects that push me beyond my
-        comfort zone and fuel continuous growth. My journey from education to
-        development has given me a unique perspective on problem-solving and a
-        passion for creating intuitive, user-focused experiences. I specialize
-        in <b>JavaScript</b>, <b>React</b>, <b>Node.js</b>, <b>Next.js</b>, and{' '}
-        <b>SQL</b>, while constantly exploring new libraries and staying current
-        with the latest web development trends. I am also skilled in{' '}
-        <b>API calls</b> and have recently delved into <b>AI</b>, exploring its
-        potential in web development. One of my recent projects, the{' '}
+        I am a former educator turned self-taught Software Engineer based in
+        Orlando, Florida. My journey from education to development has given me
+        a unique perspective on problem-solving and a passion for creating
+        intuitive, user-focused experiences. I've recently gained experience
+        working within a team of developers in an agile environment. This has
+        allowed me to explore collaborative coding practices, version control
+        with Git, and the intricacies of working on larger projects. I enjoy the
+        challenge of tackling complex problems and finding elegant solutions
+        that enhance user experience alongside a team. <br />
+        I specialize in <b>JavaScript</b>,{' '}
+        <b>React</b>, <b>Node.js</b>, <b>Next.js</b>, and <b>SQL</b>, while
+        constantly exploring new libraries and staying current with the latest
+        web development trends. <br />
+         One of my recent projects, the{' '}
         <b>LinkedIn Post Generator</b>, showcases how I leveraged <b>OpenAI</b>{' '}
         to automate content creation, demonstrating my ability to integrate
-        AI-driven solutions into my work. I&apos;m currently channeling my teaching
-        background into building a <b>JavaScript course</b> to help others
-        discover the power and possibilities of programming. If you&apos;d like to
-        learn more about this course, <b>click here</b>. This project combines
-        my love for education with my technical expertise. When I&apos;m not coding,
-        you&apos;ll find me exploring Orlando&apos;s vibrant art scene, working out, or
-        reading. Let&apos;s build something amazing <b>together</b>.
+        AI-driven solutions into my work. I&apos;m currently channeling my
+        teaching background into building a <b>JavaScript course</b> to help
+        others discover the power and possibilities of programming. This project
+        combines my love for education with my technical expertise. When
+        I&apos;m not coding, you&apos;ll find me exploring Orlando&apos;s
+        vibrant art scene, working out, or reading. <br /><br />
+        Connect with me and let&apos;s build something
+        amazing <b>together</b>.
       </p>
     </div>
   );
